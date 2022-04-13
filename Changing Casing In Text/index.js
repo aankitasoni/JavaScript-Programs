a98373ce838322118e3af2ad8228c85873222765
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2022-04-13 21:14:17 +0530

    Changing Casing in Text

--- Changing Casing In Text/index.js
+++ Changing Casing In Text/index.js
@@ -1,1 +1,16 @@
-
+/** Changing Casing in Text so that the first character of the name is Upper Case and rest character of the name is Lower Case. **/
+//1 Create a variable that stores the name that user enters via prompt.
+var name= prompt("What is your name?");
+//2 Captalise the first letter of their name.
+//a Isolate the first character.
+var firstChar = name.slice(0,1);
+//b Turn the first character to upper case.
+var upperCaseFirstChar = firstChar.toUpperCase();
+//c Isolate the rest of the name.
+var restOfName = name.slice(1,name.length);
+//d Change the rest of the name to lower case.
+restOfName = restOfName.toLowerCase();
+//e Concactenate the first character with the rest of the character.
+var capitalisedName = upperCaseFirstChar + restOfName;
+//3 Use the capitalised version of their name to greet them using an alert.
+alert("Hello, "+ capitalisedName);
