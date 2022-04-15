9d980afdd7e6fa98846b27cff9044daa20c0012c
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2022-04-15 21:54:21 +0530

    Update While Loop

---
+++ While Loop/index.js
@@ -0,0 +1,13 @@
+/** The lyrics to the 99 bottles of beer song. **/
+var numberOfBottles = 99
+while (numberOfBottles >= 1) {
+    var bottleWord = "bottle";
+    if (numberOfBottles === 1) {
+        bottleWord = "bottles";
+    } 
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+    console.log(numberOfBottles + " " + bottleWord + " of beer,");
+    console.log("Take one down, pass it around,");
+	numberOfBottles--;
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
+}
