e1b025a350728921e4df7264e06276d61a65f96b
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2022-04-17 14:11:21 +0530

    Delete Functions/Constructor Function directory

--- Functions/Constructor Function/index.js
+++
@@ -1,8 +0,0 @@
-function HouseKeeper (yearsOfExperience, name, cleaningRepertoire) {
-    this.yearsOfExperience = yearsOfExperience;
-    this.name = name;
-    this.cleaningRepertoire = cleaningRepertoire;
-}
-
-var houseKeeper1 = new HouseKeeper(20, "Tom", ["bedroom", "bathroom"]);
-console.log(houseKeeper1.name);
