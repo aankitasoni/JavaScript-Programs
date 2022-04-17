8e4ed03f7351c71f7a6ba6f5ade740bec9d4a228
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2022-04-17 16:42:56 +0530

    Update Constructor Function

--- Constructor Function/index.js
+++ Constructor Function/index.js
@@ -2,7 +2,11 @@ function HouseKeeper (yearsOfExperience, name, cleaningRepertoire) {
     this.yearsOfExperience = yearsOfExperience;
     this.name = name;
     this.cleaningRepertoire = cleaningRepertoire;
+    this.clean = function () {
+        alert("Cleaning in progress...");
+    }
 }
 
 var houseKeeper1 = new HouseKeeper(20, "Tom", ["bedroom", "bathroom"]);
+houseKeeper1.clean();
 console.log(houseKeeper1.name);
