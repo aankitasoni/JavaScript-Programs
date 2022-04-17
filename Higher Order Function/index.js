f5712f92888b31b483b9582e6b4bff659c116bac
Ankita Soni <92040840+aankitasoni@users.noreply.github.com>
2022-04-17 12:31:34 +0530

    Update Calculator

---
+++ Higher Order Function/index.js
@@ -0,0 +1,23 @@
+/** To make a Calculator. **/
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract (num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function calculator(num1, num2, operator) {
+    return operator (num1, num2);
+}
+
+calculator(2, 4, divide);
